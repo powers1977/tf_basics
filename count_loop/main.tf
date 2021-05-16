fd3cf8603a2f020553139093037c0f94078dd01b
Ryan Powers <ryan.p.powers@gmail.com>
2021-05-16 23:44:05 +0000

    Added some for loops

--- count_loop/main.tf
+++ count_loop/main.tf
@@ -8,4 +8,22 @@ resource "azurerm_resource_group" "group" {
   location = var.rg-location
 }
 
+output "resource-groups" {
 
+	#__Display name of one group
+	#value = azurerm_resource_group.group[0].name
+
+	#__Display name of all groups with splat
+	#value = azurerm_resource_group.group[*].name
+
+	#__Display name of all groups with for loop
+	#value = [for gname in azurerm_resource_group.group : gname]
+	value = [for gname in azurerm_resource_group.group : gname.name]
+}
+
+output "test" {
+  value = {
+    for info, details in azurerm_resource_group.group:
+    info => ({"name" = details.name , "location" = details.location})
+  }
+}
