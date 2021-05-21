1c866ef8804cab0913d424953255fa53b97033a0
Ryan Powers <ryan.p.powers@gmail.com>
2021-05-21 00:40:47 +0000

    Updates to add output

--- call_module/main.tf
+++ call_module/main.tf
@@ -4,6 +4,10 @@ provider "azurerm" {
 
 module "new_group" {
 	source = "../modules/create_rg"
-	group-name = "ryans_sweet_group"
+	group-name = "ryans_big_butt_group"
 	group-location = "eastus"
+}
+
+output "return_output" {
+	value = module.new_group.group_info
 }
--- modules/create_rg/main.tf
+++ modules/create_rg/main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "group" {
   name     = var.group-name
   location = var.group-location
 }
+
+output "group_info" {
+	value = azurerm_resource_group.group
+
+}
